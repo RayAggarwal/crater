d126bb8218a14b1521f6e619ad143b04ba9e80de
Raymond Aggarwal <rvaggarw@edu.uwaterloo.ca>
2019-01-15 21:02:34 -0500

    added initial database setup script

---
+++ api/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,52 @@
+-- create public tables
+CREATE TABLE IF NOT EXISTS public.owner (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  username VARCHAR(64) NOT NULL UNIQUE,
+  password VARCHAR NOT NULL,
+  applicationID VARCHAR(36),
+  PRIMARY KEY (id)
+);
+CREATE UNIQUE INDEX IF NOT EXISTS username_idx ON public.owner (username);
+
+-- tenant templates tables
+CREATE SCHEMA IF NOT EXISTS tenant_template;
+CREATE TABLE IF NOT EXISTS tenant_template.user (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  owner_id INT NOT NULL UNIQUE,
+  username VARCHAR(64) NOT NULL UNIQUE,
+  email VARCHAR(64) NOT NULL UNIQUE,
+  password VARCHAR(64) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (owner_id) REFERENCES public.owner (id)
+);
+CREATE UNIQUE INDEX IF NOT EXISTS username_idx on tenant_template.user (username);
+CREATE UNIQUE INDEX IF NOT EXISTS email_idx on tenant_template.user (email);
+
+-- trigger to create schema for new owners
+CREATE OR REPLACE FUNCTION public.add_owner_schema()
+  RETURNS TRIGGER AS
+$BODY$
+DECLARE
+  objecto TEXT;
+  buffer TEXT;
+  rec RECORD;
+BEGIN
+  EXECUTE 'CREATE SCHEMA tenant_'||new.id|| ' AUTHORIZATION crater';
+  FOR objecto IN SELECT TABLE_NAME::TEXT FROM information_schema.TABLES WHERE table_schema = 'tenant_template'
+  LOOP
+    buffer := 'tenant_' || new.id || '.' || objecto;
+    EXECUTE 'CREATE TABLE ' || buffer || ' (LIKE tenant_template.' || objecto || ' INCLUDING ALL)';
+    FOR rec IN SELECT oid, conname FROM pg_constraint WHERE contype = 'f' AND conrelid = FORMAT('tenant_template.%s', objecto)::regclass
+    LOOP
+      EXECUTE 'ALTER TABLE ' || buffer || ' ADD CONSTRAINT ' || rec.conname|| ' ' || pg_get_constraintdef(rec.oid);
+    END LOOP;
+  END LOOP;
+  RETURN NULL;
+END;
+$BODY$ LANGUAGE plpgsql;
+
+CREATE TRIGGER new_owner
+  AFTER INSERT
+  ON public.owner
+  FOR EACH ROW
+  EXECUTE PROCEDURE public.add_owner_schema();
